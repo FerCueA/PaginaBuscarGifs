7eefc2e09f3b7ba9da908db179d1ad85d10fc6e6
Aleixo <fercuea90@protonmail.com>
2025-09-03 03:13:55 +0100

    version 1.0

--- src/components/gifsCards.js
+++ src/components/gifsCards.js
@@ -1,17 +1,22 @@
 export function crearCardGifs(gif){
+
+    // CREACION DE LA CARD CON UNA DIV Y UNA CLASE ASOCIADO PARA PODER ACCEDER A LA CLASE
     const card = document.createElement('div')
     card.className='card'
+
+    //CRECAION DE LA "IMG" QUE ES DONDE ALOGAREMOS EL GIF BUSCADO
 
     const img = document.createElement('img')
     img.src = gif.images.fixed_width.url
     img.alt = gif.title
 
+    // CREACION DEL TITULO DEL GIF 
 
     const titulo = document.createElement('div');
     titulo.className = 'card-title';
     titulo.textContent = gif.title;
 
-
+    // ORDEN DE CARGA DE LAS "CREACIONES"
     card.appendChild(img);
     card.appendChild(titulo);
 
